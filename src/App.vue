761defcbc02f0bf8ede0ed7e8920e223f4234306
Divyansh Chaudhary <divyanshch@outlook.com>
2020-10-14 22:04:58 -0600

    the todo app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./layout/header";
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    Header
+  },
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
+
+/* #nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
   color: #2c3e50;
-  margin-top: 60px;
+} */
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="item.id" v-for="item in todos">
+     <TodoItem v-bind:todo="item" v-on:del-todo="$emit('del-todo',item.id)"/>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+  name: "Todos",
+  components:{
+      TodoItem
+  },
+  props: ["todos"],
+};
+</script>
+
+<style scoped>
+</style>
